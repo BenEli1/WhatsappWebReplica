c7a02df06ec8407cc79ff238220dd6e0713fcc2c
saharofe <saharofe01@gmail.com>
2022-04-03 21:43:30 +0300

    still trying

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
   return (
     <div id="maincomponent" className="container">
       <div className="row">
-        <div className="col-xl-3 col-lg-2 col-md-0 mainbox">
+        <div className="col-xl-3 col-lg-2 col-sm-1 mainbox">
         </div>
-        <div className="col-xl-6 col-lg-8 col-md-12 mainbox">
+        <div className="col-xl-6 col-lg-8 col-sm-10  mainbox">
           <Login />
         </div>
-        <div className="col-xl-3 col-lg-2 col-md-0 mainbox">
+        <div className="col-xl-3 col-lg-2 col-sm-1 mainbox">
         </div>
       </div>
     </div>
