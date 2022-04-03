ae934d64d5aef1863e50a3d7934f2139bbe9a071
Ben Eli <benelashvili@gmail.com>
2022-04-03 19:41:43 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Login from './loginPage/Login'
 function App() {
   return (<div class="container">
   <div class="row">
-    <div class="col">
-      Username: 
+  <div class="col-3" className="mainbox" >
     </div>
-    <div class="col">
-      0000
+    <div class="col-6 col-ls-12" className="mainbox">
+      <Login/>
+    </div>
+    <div class="col-3" className="mainbox">
     </div>
   </div>
 </div>);
