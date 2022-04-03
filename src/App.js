fa16392f3fe90955b151a5f6a600c1c5286518fd
saharofe <saharofe01@gmail.com>
2022-04-03 21:10:30 +0300

    try color background

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Login from './loginPage/Login'
 function App() {
   return (
-    <div class="container">
-      <div class="row">
+    <div id="maincomponent" className="container">
+      <div className="row">
         <div className="col-xl-3 col-lg-2 col-md-0 mainbox">
         </div>
         <div className="col-xl-6 col-lg-8 col-md-12 mainbox">
