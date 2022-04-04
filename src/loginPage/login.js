41c82f4bba8880b8711b9ecba30ec03f44db9c62
saharofe <saharofe01@gmail.com>
2022-04-04 20:43:27 +0300

    /

--- src/loginPage/login.js
+++ src/loginPage/login.js
@@ -1,19 +1,23 @@
+import { BrowserRouter, Link } from "react-router-dom";
+
 function Login(){
     return(
-    <div>    
-    <div class="input-group flex-nowrap">
-    <span class="input-group-text" id="addon-wrapping">Username</span>
-    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"></input>
-    </div>
-    <div class="input-group flex-nowrap">
-    <span class="input-group-text" id="addon-wrapping">Password</span>
-    <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping"></input>
-  </div>
-  <div>
-  <button type="button" class="btn btn-light input-group border-secondary">Login</button>
-  </div>
-  <button type="button" id="newaccount" class="btn btn-light input-group">Creatre a new account</button>
-  </div>
+      <div>
+        <form>
+          <div className="input-group flex-nowrap">
+            <span className="input-group-text" name="userNameLog" id="username-login">Username</span>
+            <input type="text" required className="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"></input>
+          </div>
+          <div className="input-group flex-nowrap">
+            <span className="input-group-text" id="password-login">Password</span>
+            <input type="password" required className="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping"></input>
+          </div>
+          <div>
+            <button type="button submit" id="submitLogIn" className="btn btn-light input-group border-secondary">Login</button>
+          </div>
+        </form>
+        <Link to='/register' type="button" href="/register" id="newaccount" className="btn btn-light input-group">Creatre a new account</Link>
+      </div>
 );
 }
 export default Login;
