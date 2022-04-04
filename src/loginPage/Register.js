55673077711ee5fda847e37f3041806e88525c36
saharofe <saharofe01@gmail.com>
2022-04-04 15:05:02 +0300

    replace class to className

--- src/loginPage/Register.js
+++ src/loginPage/Register.js
@@ -4,39 +4,39 @@ function Register(){
     return(
         <div>
             <form>
-                <div class="input-group flex-nowrap">
-                    <span class="input-group-text" id="username-register">Username</span>
-                    <input type="text" required class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"></input>
+                <div className="input-group flex-nowrap">
+                    <span className="input-group-text" id="username-register">Username</span>
+                    <input type="text" required className="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"></input>
                 </div>
 
-                <div class="input-group flex-nowrap">
-                    <span class="input-group-text" id="nickname-register">Nickname</span>
-                    <input type="text" required class="form-control" placeholder="Nickname" aria-label="Nickname" aria-describedby="addon-wrapping"></input>
+                <div className="input-group flex-nowrap">
+                    <span className="input-group-text" id="nickname-register">Nickname</span>
+                    <input type="text" required className="form-control" placeholder="Nickname" aria-label="Nickname" aria-describedby="addon-wrapping"></input>
                 </div>
 
-                <div class="input-group flex-nowrap">
-                    <span class="input-group-text" id="password-register">Password</span>
-                    <input type="password" required class="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping"></input>
+                <div className="input-group flex-nowrap">
+                    <span className="input-group-text" id="password-register">Password</span>
+                    <input type="password" required className="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping"></input>
                 </div>
 
-                <div class="input-group flex-nowrap">
-                    <span class="input-group-text" id="password-again-register">Password Verification</span>
-                    <input type="password" required class="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping"></input>
+                <div className="input-group flex-nowrap">
+                    <span className="input-group-text" id="password-again-register">Password Verification</span>
+                    <input type="password" required className="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping"></input>
                 </div>
 
-                <div class="input-group flex-nowrap">
-                    <div id="selectImage" for="imageFromUser" class="form-label form-control bg-light">Select Image</div>
+                <div className="input-group flex-nowrap">
+                    <div id="selectImage" className="form-label form-control bg-light">Select Image</div>
                 </div>
-                <div id="fileLoad" class="input-group flex-nowrap">
-                    <input type="file" required id="imageFromUser" class="form-control" accept="image/*" placeholder="Image" aria-label="Image" aria-describedby="addon-wrapping"></input>
+                <div id="fileLoad" className="input-group flex-nowrap">
+                    <input type="file" required id="imageFromUser" className="form-control" accept="image/*" placeholder="Image" aria-label="Image" aria-describedby="addon-wrapping"></input>
                 </div>
 
                 <div>
-                    <button type="button submit" class="btn btn-light input-group border-secondary">Sign Up</button>
+                    <button type="button submit" className="btn btn-light input-group border-secondary">Sign Up</button>
                 </div>
             </form>
             <div>
-                <Link to="/" type="button" id="newaccount-register" class="btn btn-light input-group">Already have an account?</Link>
+                <Link to="/" type="button" id="newaccount-register" className="btn btn-light input-group">Already have an account?</Link>
             </div>
         </div>
     );
