3b0bcaa9240e525705abf3f52436e5be9f2c9f3f
saharofe <saharofe01@gmail.com>
2022-04-08 18:11:06 +0300

    add nav-bar

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,29 +1,31 @@
 import ChatUSerCard from "./chatUserCard";
+import NavBarChat from "./NavBarChat";
 
 function Chat(){
     return(
         <div className="container">
-        <div className="row">
-          <div className="col-xl-4 col-lg-4 col-sm-4">
-            <ChatUSerCard nickName="sahar" />
-            <ChatUSerCard nickName="sahar" />
-            <ChatUSerCard nickName="sahar" />
-            <ChatUSerCard nickName="sahar" />
-            <ChatUSerCard nickName="sahar" />
-            <ChatUSerCard nickName="sahar" />
-            <ChatUSerCard nickName="sahar" />
-            <ChatUSerCard nickName="sahar" />
-            <ChatUSerCard nickName="sahar" />
-            <ChatUSerCard nickName="sahar" />
-            <ChatUSerCard nickName="sahar" />
-            <ChatUSerCard nickName="sahar" />
-          </div>
-          <div className="col-xl-7 col-lg-8 col-sm-10">
-          </div>
-          <div className="col-xl-1 col-lg-0 col-sm-0">
-          </div>
+            <div className="row">
+                <div className="col-xl-4 col-lg-4 col-sm-4">
+                    <NavBarChat />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                </div>
+                <div className="col-xl-7 col-lg-8 col-sm-10">
+                </div>
+                <div className="col-xl-1 col-lg-0 col-sm-0">
+                </div>
+            </div>
         </div>
-      </div>
     );
 }
 
