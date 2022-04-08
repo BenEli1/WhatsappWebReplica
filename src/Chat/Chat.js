977b82693c6ff7ec3da46cb117184348be04e9fc
saharofe <saharofe01@gmail.com>
2022-04-08 18:38:30 +0300

    little changes

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -5,7 +5,7 @@ function Chat(){
     return(
         <div className="container">
             <div className="row">
-                <div className="col-xl-4 col-lg-4 col-sm-4">
+                <div className="col-xl-4 col-lg-4 col-sm-4 col-4">
                     <NavBarChat />
                     <ChatUSerCard nickName="sahar" />
                     <ChatUSerCard nickName="sahar" />
@@ -20,9 +20,9 @@ function Chat(){
                     <ChatUSerCard nickName="sahar" />
                     <ChatUSerCard nickName="sahar" />
                 </div>
-                <div className="col-xl-7 col-lg-8 col-sm-10">
+                <div className="col-xl-8 col-lg-8 col-sm-8 col-8">
                 </div>
-                <div className="col-xl-1 col-lg-0 col-sm-0">
+                <div className="col-xl-0 col-lg-0 col-sm-0 col-6">
                 </div>
             </div>
         </div>
--- src/Chat/chatUserCard.js
+++ src/Chat/chatUserCard.js
@@ -4,10 +4,10 @@ function ChatUSerCard({nickName, image}){
     return(
         <div className="btn-outline-light chat_list border-dark">
             <div className="chat_people row">
-                <div className="col-2"> 
+                <div className="col-md-2 col-1"> 
                     <img src="saharofe.jpg" className="imgChat img-fluid"></img>
                 </div>
-                <div className="chat_ib col-10">
+                <div className="chat_ib col-md-10">
                     <h5>{nickName}<span className="chat_date">Dec 25</span></h5>
                     <p>hi, how are you?</p>
                 </div>
