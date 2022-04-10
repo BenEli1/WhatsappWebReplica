021b2bac9927fe598b218902c7ce924c194958a1
Ben Eli <benelashvili@gmail.com>
2022-04-10 23:35:06 +0300

    -

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,7 +1,7 @@
 import ChatUSerCard from "./chatUserCard";
 import NavBarChat from "./NavBarChat";
 function Chat(){
-    return(0);}/*
+    return(
         <div className="container" id="Chat">
             <div className="row">
                 <div className="col-xl-4 col-lg-4 col-sm-4 col-4" id="leftChat">
@@ -12,17 +12,22 @@ function Chat(){
                     <ChatUSerCard nickName="sahar" />
                     <ChatUSerCard nickName="sahar" />
                     <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
+                    <ChatUSerCard nickName="sahar" />
                     </div>
                 </div>
                 <div className="col-xl-8 col-lg-8 col-sm-8 col-8" id="rightChat">
-                <div className="input-group flex-nowrap">
-            <span className="input-group-text" name="chat">Enter</span>
-            <input type="text" id="chat" required className="form-control" placeholder="..." aria-label="Enter" aria-describedby="addon-wrapping"></input>
-          </div>
                 </div>
                 <div className="col-xl-0 col-lg-0 col-sm-0 col-0">
                 </div>
             </div>
-        </div>*/
+        </div>
+    );
+}
 
 export default Chat;
