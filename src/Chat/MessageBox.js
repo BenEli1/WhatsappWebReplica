2e918b2cb571c48c35016f27ecb67b3a98f9ad1f
Ben <benelashvlili@gmail.com>
2022-04-12 11:16:06 +0300

    MessageBox

---
+++ src/Chat/MessageBox.js
@@ -0,0 +1,15 @@
+
+function MessageBox(){
+    return(
+
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <button class="btn btn-outline-secondary" type="button">Button</button>
+  </div>
+  <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1"></input>
+</div>
+    );
+}
+
+export default MessageBox;
+
