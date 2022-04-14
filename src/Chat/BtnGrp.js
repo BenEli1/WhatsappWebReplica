76a683bea4ed7123fbcbed6277ec7ae825f6b479
Ben <benelashvlili@gmail.com>
2022-04-14 16:42:37 +0300

    mic

--- src/Chat/BtnGrp.js
+++ src/Chat/BtnGrp.js
@@ -14,102 +14,9 @@ function BtnGrp({addMessage}) {
                   let image = URL.createObjectURL(input.files[0]);
                   addMessage(image, "image");
               };
-        
-    
-      }
 
-      function importVoive(){
-        let audioIN = { audio: true };
-      
-        // Access 
-        navigator.mediaDevices.getUserMedia(audioIN)
-     
-          // 'then()' method returns a Promise
-          .then(function (mediaStreamObj) {
-            // Connect the media stream to the
-            // first audio element
-            let audio = document.querySelector('audio');
-            //returns the recorded audio via 'audio' tag
-            // 'srcObject' is a property which
-            // takes the media object
-            // This is supported in the newer browsers
-            if ("srcObject" in audio) {
-              audio.srcObject = mediaStreamObj;
-            }
-            else {   // Old version
-              audio.src = window.URL
-                .createObjectURL(mediaStreamObj);
-            }
-            // It will play the audio
-            audio.onloadedmetadata = function (ev) {
-              // Play the audio in the 2nd audio
-              // element what is being recorded
-              audio.play();
-            };
-     
-            // Start record
-            let start = document.getElementById('btnStart');
-     
-            // Stop record
-            let stop = document.getElementById('btnStop');
-     
-            // 2nd audio tag for play the audio
-            let playAudio = document.getElementById('adioPlay');
-     
-            // This is the main thing to recorded
-            // the audio 'MediaRecorder' API
-            let mediaRecorder = new MediaRecorder(mediaStreamObj);
-            // Pass the audio stream
-     
-            // Start event
-            start.addEventListener('click', function (ev) {
-              mediaRecorder.start();
-              // console.log(mediaRecorder.state);
-            })
-     
-            // Stop event
-            stop.addEventListener('click', function (ev) {
-              mediaRecorder.stop();
-              // console.log(mediaRecorder.state);
-            });
-     
-            // If audio data available then push
-            // it to the chunk array
-            mediaRecorder.ondataavailable = function (ev) {
-              dataArray.push(ev.data);
-            }
-     
-            // Chunk array to store the audio data
-            let dataArray = [];
-     
-            // Convert the audio data in to blob
-            // after stopping the recording
-            mediaRecorder.onstop = function (ev) {
-     
-              // blob of type mp3
-              let audioData = new Blob(dataArray,
-                        { 'type': 'audio/mp3;' });
-               
-              // After fill up the chunk
-              // array make it empty
-              dataArray = [];
-     
-              // Creating audio url with reference
-              // of created blob named 'audioData'
-              let audioSrc = window.URL
-                  .createObjectURL(audioData);
-     
-              // Pass the audio url to the 2nd video tag
-              playAudio.src = audioSrc;
-            }
-          })
-     
-          // If any error occurs then handles the error
-          .catch(function (err) {
-            console.log(err.name, err.message);
-          });
       }
-    
+     
       function importVideo() {
         let input = document.createElement('input');
         input.setAttribute('id','loadVideo')
@@ -122,6 +29,31 @@ function BtnGrp({addMessage}) {
                   addMessage(video, "video");
               };
         
+      }
+      function importVoice(){
+        let audioIN = { audio: true };
+        navigator.mediaDevices.getUserMedia(audioIN)
+          .then(function (mediaStreamObj) {
+            
+            let stop = document.getElementById('btnStop-');
+            let mediaRecorder = new MediaRecorder(mediaStreamObj);
+            mediaRecorder.start();
+            stop.addEventListener('click', function (ev) {
+              mediaRecorder.stop();
+            },{once:true});
+            mediaRecorder.ondataavailable = function (ev) {
+              dataArray.push(ev.data);
+            }
+            let dataArray = [];
+            mediaRecorder.onstop = function (ev) {
+              let audioData = new Blob(dataArray,
+                        { 'type': 'audio/mp3;' });
+              dataArray = [];
+              let audioSrc = window.URL
+                  .createObjectURL(audioData);
+              addMessage(audioSrc, "voice");
+            }
+          })
       }
 
     return (
@@ -136,10 +68,13 @@ function BtnGrp({addMessage}) {
                     <i className="material-icons">videocam</i>
                 </button>
 
-                <button className="btn btn-secondary " type="button" onClick={importVoive} id="button-addon2">
+                <button className="btn btn-secondary " type="button" onClick={importVoice} id="btnStart-">
                     <i className="material-icons">keyboard_voice</i>
                 </button>
 
+                <button className="btn btn-secondary " type="button" id="btnStop-">
+                    stop
+                </button>
 
             </div>
         </div>
